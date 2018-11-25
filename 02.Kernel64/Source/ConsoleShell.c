5f51d81ec94083cfe2209809dc43d9f79e9a46b5
lionem2018 <lionem2018@gmail.com>
2018-11-24 21:10:33 -0800

    modify print message

--- 02.Kernel64/Source/ConsoleShell.c
+++ 02.Kernel64/Source/ConsoleShell.c
@@ -865,7 +865,7 @@ static void kShowTaskList( const char* pcParameterBuffer )
                      pstTCB->qwFlags, kGetListCount( &( pstTCB->stChildThreadList ) ) );
             kPrintf( "    Parent PID[0x%Q], Memory Address[0x%Q], Size[0x%Q]\n",
                     pstTCB->qwParentProcessID, pstTCB->pvMemoryAddress, pstTCB->qwMemorySize );
-            kPrintf( "    Ticket[%d], Schedule Time[%d]\n", pstTCB->ticket, pstTCB->execCount);
+            kPrintf( "    Ticket[%d], CPU Time[%d]\n", pstTCB->ticket, pstTCB->execCount);
         }
     }
 }
@@ -1174,7 +1174,7 @@ static void kCheckTask( const char* pcParameterBuffer )
                             pstTCB->qwFlags, kGetListCount( &( pstTCB->stChildThreadList ) ) );
                     kPrintf( "    Parent PID[0x%Q], Memory Address[0x%Q], Size[0x%Q]\n",
                             pstTCB->qwParentProcessID, pstTCB->pvMemoryAddress, pstTCB->qwMemorySize );
-                    kPrintf( "    Ticket[%d], Schedule Time[%d]\n", pstTCB->ticket, pstTCB->execCount);
+                    kPrintf( "    Ticket[%d], CPU Time[%d]\n", pstTCB->ticket, pstTCB->execCount);
 
                     //pstTCB->execCount = 0;
                 }
